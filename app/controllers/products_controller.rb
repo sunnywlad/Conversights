beb854de47771f7500c462be118d74a88d9fdca4
Wladimir Blaise <wld.world@tuta.io>
2026-04-06 14:11:06 +0200

    created basic products index and basic dashboard

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = current_user.products
+  end
+
+  def show
+    @product = current_user.products.find(params[:id])
+    @dashboard_cards = @product.dashboard_cards
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :dashboard_cards, dependent: :destroy
+  has_many :chats, dependent: :destroy
 end
